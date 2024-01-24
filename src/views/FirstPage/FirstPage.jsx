dd40a45633eb1aac41592aa1334de0a9a04e3ffe
david ben gurion <58559398+DavidBenGurion@users.noreply.github.com>
2024-01-25 02:48:19 +0700

    upload file 2

---
+++ src/views/FirstPage/FirstPage.jsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+import styles from '../../styles/first-page.module.css';
+import axiosClient from "../../axios-client";
+import { useNavigate } from "react-router-dom";
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content';
+const FirstPage=()=>{
+  const [photoList,setPhotoList]=useState([]);
+  const navigate = useNavigate();
+  const swalLoading = withReactContent(Swal)
+  useEffect(()=>{
+    swalLoading.fire({
+      title: <p>Loading</p>,
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+      allowEnterKey: false,
+      didOpen: () => {
+       swalLoading.showLoading()
+      },
+  })
+    axiosClient.get('?method=flickr.tags.getHotList&api_key=7864a899300716253e64d678a63f6323&period=year&count=20&format=json&nojsoncallback=1').then(({data})=>{
+      var dataPhotoList=[];
+      console.log(data);
+      const phototag=data.hottags.tag;
+      // console.log(data.photos);
+      for (let index = 4; index < 8; index++) {
+          dataPhotoList.push({src:`https://live.staticflickr.com/${phototag[index].thm_data.photos.photo[0].server}/${phototag[index].thm_data.photos.photo[0].id}_${phototag[index].thm_data.photos.photo[0].secret}_w.jpg`,
+                            id:phototag[index].thm_data.photos.photo[0].id,
+                            secret:phototag[index].thm_data.photos.photo[0].secret})
+      }
+      // console.log(dataPhotoList)
+      setPhotoList(dataPhotoList)
+      swalLoading.close();
+    })
+  },[])
+  const redirectToDetailPhoto=(photoId,secret)=>{
+    navigate(`/detil/${photoId}/${secret}`)
+ }
+  return(
+    <React.Fragment>
+      <div style={{width:"90%"}} className="mx-auto ">
+        <div className="d-inline-block pe-2" style={{width:"40%"}}>
+            <div>
+                <h1 className={"w-100 text-end "+styles.firstPage}>Flckr</h1>
+            </div>
+           <div>
+                <p className={styles.firstPage+" w-100 text-end"}>Art, Hobbies, and Fashion</p>
+                <div style={{borderTop:"1px solid black",borderBottom:"1px solid black",}} className="py-2">
+                    <h1 className={styles.firstPage+" w-100 text-end"}>This website have some photo</h1>
+                </div>
+           </div>
+        </div>
+        <div className="d-inline-block "  style={{width:"60%"}}>
+             {
+                photoList.length>0?(
+                    <div className="box_image_container my-1" style={{width:"fit-content",backgroundColor:"#B80000"}}>
+                          <div style={{width:"fit-content"}}>
+                            <div className={"d-inline-block bg-light m-2 align-top  "+styles.box_image} >
+                                <img src={photoList[0].src} className={"w-100 h-100 "+styles.img} style={{cursor:"pointer"}} onClick={()=>redirectToDetailPhoto(photoList[0].id,photoList[0].secret)}/>
+                            </div>
+                            <div className={"d-inline-block bg-light m-2 align-top "+styles.box_image} >
+                              <img src={photoList[1].src} className={"w-100 h-100 "+styles.img}style={{cursor:"pointer"}} onClick={()=>redirectToDetailPhoto(photoList[1].id,photoList[1].secret)}/>
+                            </div>
+                          </div>
+                          <div style={{width:"fit-content"}} className="m-2">
+                            <div className={"d-inline-block bg-light me-2 align-top "+styles.box_image} >
+                              <img src={photoList[2].src} className={"w-100 h-100 "+styles.img} style={{cursor:"pointer"}} onClick={()=>redirectToDetailPhoto(photoList[2].id,photoList[2].secret)}/>
+                            </div>
+                            <div className={"d-inline-block bg-light m-2 align-top "+styles.box_image} >
+                              <img src={photoList[3].src} className={"w-100 h-100 "+styles.img} style={{cursor:"pointer"}} onClick={()=>redirectToDetailPhoto(photoList[3].id,photoList[3].secret)}/>
+                            </div>
+                          </div>
+                    </div>
+                ):(null)
+             }
+             
+          </div>
+      </div>
+     
+    </React.Fragment>
+  )
+}
+export default FirstPage;
---
+++ src/views/Main/HotTag.jsx
@@ -0,0 +1,17 @@
+import styles from '../../styles/hot-tag.module.css';
+import BoxPhotoHotTag from './BoxPhotoHotTag';
+const HotTag=(props)=>{
+   
+  return(
+    <div className="bg-light mt-5 p-1 rounded shadow">
+      <div>
+         <h3 id={styles.title_hot_tag}>Hot tags of the week </h3>
+          {props.hotTag.map((hotTag)=>(
+            <BoxPhotoHotTag hotTag={hotTag}/>
+          ))
+        } 
+      </div>
+    </div>
+  )
+}
+export default HotTag;
